fe42f02db8fbb8201b6ee07f8257c77e622accb0
Songblade <sgreenga@mail.yu.edu>
2022-02-20 22:41:42 -0500

    I am not entirely sure if I fixed things, but if all goes well, I will be uploading a HashTable with put implemented

--- DataStructures/project/stage1/src/main/java/edu/yu/cs/com1320/project/impl/HashTableImpl.java
+++ DataStructures/project/stage1/src/main/java/edu/yu/cs/com1320/project/impl/HashTableImpl.java
@@ -2,13 +2,25 @@ package  edu.yu.cs.com1320.project.impl;
 
 import edu.yu.cs.com1320.project.HashTable;
 
+// Right now, I have a problem that I changed my file path halfway through and now IntelliJ lost track of the project
+// But I can't tell if the problem is in IntelliJ or in Maven, so I don't know what to fix
+// Hopefully, tomorrow we will learn Maven and I will learn how to fix the problem
 public class HashTableImpl<Key, Value> implements HashTable {
 
+    private ChainLink[] table; // the table in the HashTable
     // This is the constructor to create a new HashTable
     public HashTableImpl() {
         // I need to create the array
-        // Before I do anything else, I need to add another file to the package that can act as a chain list
-        // In fact, I think I will call it ChainList
+        // The array will be an array of ChainLinks
+        // It will have a length of 5, as required
+        // I will make a second constructor, private for now, that accepts a constructor of any length
+        // Just in case we need that in the future, I can just make it public
+        this(5);
+    }
+
+    private HashTableImpl(int length) {
+        table = new ChainLink[length];
+        // I feel like there should be something else here, but can't think of it
     }
 
     /**
@@ -16,7 +28,7 @@ public class HashTableImpl<Key, Value> implements HashTable {
      * @return the value that is stored in the HashTable for k, or null if there is no such key in the table
      */
     @Override
-    public Object get(Object k) {
+    public Value get(Key k) {
         return null;
     }
 
@@ -27,8 +39,14 @@ public class HashTableImpl<Key, Value> implements HashTable {
      * @return if the key was already present in the HashTable, return the previous value stored for the key. If the key was not already present, return null.
      */
     @Override
-    public Object put(Object k, Object v) {
+    public Value put(Key k, Value v) {
+        ChainLink link = table[]
         return null;
+    }
+
+    // returns a hashcode compatible with my length-5 table
+    private int hashFunction(Key k) {
+        return Math.abs(k.hashCode()) % table.length;
     }
 
     protected class ChainLink<Key, Value> {
