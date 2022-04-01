69ae85ce7f5deee142c9b803fbc4f6eb34589021
Songblade <sgreenga@mail.yu.edu>
2022-04-01 16:09:17 -0400

    Began uploading files for stage 3

---
+++ DataStructures/project/stage3/src/main/java/edu/yu/cs/com1320/project/stage3/impl/DocumentImpl.java
@@ -0,0 +1,118 @@
+package edu.yu.cs.com1320.project.stage2.impl;
+
+import edu.yu.cs.com1320.project.stage2.Document;
+
+import java.net.URI;
+import java.util.Arrays;
+
+public class DocumentImpl implements Document {
+
+    private URI uri; // the URI of the document
+    private String text; // the text of the document, null if not applicable
+    private byte[] binaryData; // the data of the document, null if not applicable
+
+    // constructor that uses text
+    public DocumentImpl(URI uri, String text) {
+        validateURI(uri);
+        if (text == null) {
+            throw new IllegalArgumentException("text is null");
+        } // Piazza says we don't need to worry about a blank URI
+        if (text.equals("")) {
+            throw new IllegalArgumentException("text is blank");
+        }
+        this.uri = uri;
+        this.text = text;
+    }
+
+    public DocumentImpl(URI uri, byte[] binaryData) {
+        validateURI(uri);
+        if (binaryData == null) {
+            throw new IllegalArgumentException("binary data is null");
+        }
+        if (binaryData.length == 0) {
+            throw new IllegalArgumentException("binary data is empty");
+        }
+        this.uri = uri;
+        this.binaryData = Arrays.copyOf(binaryData, binaryData.length);
+    }
+
+    // if the URI being inputted is null or empty, this will throw errors for my constructors
+    private void validateURI(URI uri) {
+        if (uri == null) {
+            throw new IllegalArgumentException("URI is null");
+        }
+        if (uri.toASCIIString().isBlank()) {
+            throw new IllegalArgumentException("URI is blank");
+        }
+    }
+
+    /**
+     * @return content of text document
+     */
+    @Override
+    public String getDocumentTxt() {
+        return text; // if not applicable, will already be null
+    }
+
+    /**
+     * @return content of binary data document
+     */
+    @Override
+    public byte[] getDocumentBinaryData() {
+        if (binaryData == null) {
+            return null;
+        }
+        return Arrays.copyOf(binaryData, binaryData.length);
+    }
+
+    /**
+     * @return URI which uniquely identifies this document
+     */
+    @Override
+    public URI getKey() {
+        return uri;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = uri.hashCode();
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        result = 31 * result + Arrays.hashCode(binaryData); // no error thrown if null, just adds 0
+        return result;
+    }
+
+    @Override
+    public boolean equals (Object other) {
+        if (other == null) {
+            return false;
+        }
+        if (this == other) {
+            return true;
+        }
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        // equality is determined by hashCode, once same class
+        return this.hashCode() == other.hashCode();
+    }
+
+    /*
+    //make sure you get rid of this, or you will fail
+    // I am leaving this code here, in case I ever want the print again
+    @Override
+    public String toString() {
+        String value = uri.toString() + ": ";
+        if (binaryData == null) {
+            value += text;
+        } else {
+            if (binaryData.length < 10) {
+                value += Arrays.toString(binaryData);
+            } else {
+                value += Arrays.toString(Arrays.copyOf(binaryData, 10)) + " and more";
+            }
+        }
+        return value;
+    }
+     */
+
+}
---
+++ DataStructures/project/stage3/src/main/java/edu/yu/cs/com1320/project/stage3/impl/DocumentStoreImpl.java
@@ -0,0 +1,183 @@
+package edu.yu.cs.com1320.project.stage2.impl;
+
+import edu.yu.cs.com1320.project.stage2.*;
+import edu.yu.cs.com1320.project.*;
+import edu.yu.cs.com1320.project.impl.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+public class DocumentStoreImpl implements DocumentStore {
+    // must use HashTableImpl to store documents
+    private HashTable<URI, Document> table;
+    private Stack<Command> commandStack;
+
+    //This shouldn't do much, just set up the HashTable
+    public DocumentStoreImpl() {
+        table = new HashTableImpl<>();
+        commandStack = new StackImpl<>();
+    }
+
+    /**
+     * @param input  the document being put
+     * @param uri    unique identifier for the document
+     * @param format indicates which type of document format is being passed
+     * @return if there is no previous doc at the given URI, return 0. If there is a previous doc, return the hashCode of the previous doc. If InputStream is null, this is a delete, and thus return either the hashCode of the deleted doc or 0 if there is no doc to delete.
+     * @throws IOException              if there is an issue reading input
+     * @throws IllegalArgumentException if uri or format are null
+     */
+    @Override
+    public int putDocument(InputStream input, URI uri, DocumentFormat format) throws IOException {
+        if (uri == null) {
+            throw new IllegalArgumentException("URI is null");
+        }
+        if (input != null && format == null) { // if input is null, then this is a delete, and the format
+            // doesn't matter
+            throw new IllegalArgumentException("format is null");
+        }
+
+        //this part deals with the adding the command to the stack
+        //commandStack.push(new Command(uri, () -> ()));
+        // since I need the Command added to add the old doc
+        int oldHash; // the old hashcode, to be returned
+        if (table.get(uri) == null) { // I must find it now, because if it is a delete, I will be returning soon
+            oldHash = 0; // I don't want problems getting the hashcode of a null element
+        } else {
+            oldHash = table.get(uri).hashCode();
+        }
+        // this adds the appropriate command to the command stack, along with saying what to add back
+
+        Document previousDoc = table.get(uri);
+        commandStack.push(new Command(uri, (uri1) -> {
+            // if previousDoc is null, HashTable will delete it for me
+            table.put(uri, previousDoc);
+            return true;
+        }));
+
+        if (input == null) { // deleting the document, if that is what was asked
+            table.put(uri, null);
+            return oldHash;
+        }
+
+        // everything from here on in should only happen if I have a valid document
+        table.put(uri, readDataToDocument(input, uri, format));
+        return oldHash;
+    }
+
+    // So I don't have to worry about having a putDocument method too long, I am moving the code that gets a
+    // document from the input stream to a separate method
+    private Document readDataToDocument(InputStream input, URI uri, DocumentFormat format) throws IOException {
+        /*Your code will receive documents as an InputStream and the document’s key as an instance of URI. When a document
+        is added to your document store, you must do the following:
+        a. Read the entire contents of the document from the InputStream into a byte[]
+        b. Create an instance of DocumentImpl with the URI and the String or byte[]that was passed to you.
+        c. Insert the Document object into the hash table with URI as the key and the Document object as the value
+        d. Return the hashCode of the previous document that was stored in the hashTable at that URI, or zero if there was
+        none*/
+        byte[] data = input.readAllBytes(); // making the array just big enough to read all the data
+        if (format == DocumentFormat.TXT) {
+            return new DocumentImpl(uri, new String(data));
+        } else { // if the format is binary
+            return new DocumentImpl(uri, data);
+        }
+    }
+
+    /**
+     * @param uri the unique identifier of the document to get
+     * @return the given document
+     */
+    @Override
+    public Document getDocument(URI uri) {
+        return table.get(uri); // if the value is null, this will return null
+    }
+
+    /**
+     * @param uri the unique identifier of the document to delete
+     * @return true if the document is deleted, false if no document exists with that URI
+     */
+    @Override
+    public boolean deleteDocument(URI uri) {
+        if (uri == null) {
+            return false; // not deleting anything, because nothing to delete
+        }
+        Document previousDoc = table.get(uri);
+        commandStack.push(new Command(uri, (uri1) -> {
+            // if previousDoc is null, HashTable will delete it for me
+            table.put(uri, previousDoc);
+            return true;
+        }));
+        // this has to be after adding the command to the stack, because it is still supposed to add a command
+        // even if it does nothing
+        if (table.get(uri) == null) {
+            return false;
+        }
+        // if there is something to delete
+        table.put(uri, null);
+        return true;
+    }
+
+    /**
+     * undo the last put or delete command
+     *
+     * @throws IllegalStateException if there are no actions to be undone, i.e. the command stack is empty
+     */
+    @Override
+    public void undo() throws IllegalStateException {
+        if (commandStack.peek() == null) {
+            throw new IllegalStateException("No commands to undo");
+        }
+        // I remove the top command and undo it, and hope my lambda stuff works
+        commandStack.pop().undo();
+    }
+
+    /**
+     * undo the last put or delete that was done with the given URI as its key
+     *
+     * @param uri
+     * @throws IllegalStateException if there are no actions on the command stack for the given URI
+     */
+    @Override
+    public void undo(URI uri) throws IllegalStateException {
+        if (commandStack.peek() == null) {
+            throw new IllegalStateException("No commands to undo");
+        }
+        Stack<Command> helperStack = new StackImpl<>(); // to put stuff on
+        // I go through each command on the stack and examine it
+        // if it is not the command I am looking for, I put it on helperStack
+        // if it is the command I am looking for, I activate it
+        // if I never find the command, I throw an ISE
+        // After activating it or before leaving with the ISE, I put all the stuff from the helperStack back on the main stack
+        Command command;
+        boolean undone = false;
+        try {
+            do { // I don't need a do-while, a regular while would do, but I ended up with this and see no problems
+                command = commandStack.pop();
+                if (command == null) {
+                    throw new IllegalStateException("No commands with the uri \"" + uri + "\" to be undone");
+                }
+                if (command.getUri().equals(uri)) { // if we found our command
+                    command.undo();
+                    undone = true;
+                } else { // if this is a dud that is not getting undone
+                    helperStack.push(command);
+                }
+            } while (!undone);
+        } finally {
+            restackStack(helperStack);
+        }
+
+    }
+
+    // this method is called at the end of undo(URI) to put everything back on the stack
+    private void restackStack(Stack<Command> helperStack) {
+        Command command;
+        do { // not regular while loop, because command could be null the first time from before, and
+            // that should not cause any problems
+            command = helperStack.pop();
+            if (command != null) { // if this is the first command that was undone, it will be null
+                commandStack.push(command);
+            }
+        } while (command != null);
+    }
+}
