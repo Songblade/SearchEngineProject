9a8871382a45b76f96a63f2a830f53c4d74c1769
Songblade <sgreenga@mail.yu.edu>
2022-05-26 09:38:20 -0400

    I created DocumentStoreTest, and moved my plans to that document.

--- DataStructures/project/stage5/src/test/java/edu/yu/cs/com1320/project/stage5/DocumentPersistenceManagerTest.java
+++ DataStructures/project/stage5/src/test/java/edu/yu/cs/com1320/project/stage5/DocumentPersistenceManagerTest.java
@@ -386,12 +386,4 @@ public class DocumentPersistenceManagerTest {
         assertEquals(wordMap, deserialized.getWordMap());
     }
 
-    /*
-    My plans:
-        Then I will go through DocumentStore to make insertion and deletion use the BTree
-        Then I will make sure that the trie stuff works with the BTree correctly, and doesn't store docs in the trie
-        Then I will make sure that undo works with the BTree correctly
-        Then I will make sure that memory usage works correctly, including all the horrible stuff I am hearing about
-            interacting with the Heap correctly
-     */
 }
